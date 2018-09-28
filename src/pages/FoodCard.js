8055849db7477b73be1d02afe1afbad52b802cff
Vishal Karande <vishalkarande4@gmail.com>
2018-09-28 01:51:39 -0400

    picker added

--- src/pages/FoodCard.js
+++ src/pages/FoodCard.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import {View, Text, Image, ActivityIndicator} from 'react-native'
+import {View, Text, Picker, ActivityIndicator} from 'react-native'
 import {Actions} from 'react-native-router-flux'
 import { Card, Header, Icon } from 'react-native-elements'
 //import NumberInput from 'rn-number-input';
@@ -7,12 +7,13 @@ import { Card, Header, Icon } from 'react-native-elements'
 class FoodCard extends Component {
 
     state ={
-        showLoader:false
-        
+        showLoader:false,
+        servingSize:1,
+        news:[]
     }
 
     componentWillMount = () => {
-        this.setState({news:[],showLoader:true})
+        this.setState({showLoader:true})
     }
 
     componentDidMount = () => {
@@ -42,7 +43,7 @@ class FoodCard extends Component {
                 <Header
                     outerContainerStyles={{height:60,backgroundColor:"#0F084B"}}
                     leftComponent={backButton}
-                    centerComponent={{ text: this.state.news.title, style: { color: '#fff',fontSize:15 }}}
+                    centerComponent={{ text: "Add Food", style: { color: '#fff',fontSize:15 }}}
                 />
                 {
                     (this.state.showLoader == true) ? <ActivityIndicator size="large" color="#0000ff" /> : 
@@ -56,13 +57,24 @@ class FoodCard extends Component {
                                      }}>Name</Text>
                                  </Card>
                                  <Card style={{flexDirection:'column'}}>
-                                     <Text style={{
-                                         color: "maroon",
-                                         fontSize: 15,
-                                         marginBottom: 5
-                                     }}>ServingSize</Text>
-                                     
-                             
+                                        <Text style={{
+                                            color: "maroon",
+                                            fontSize: 15,
+                                            marginBottom: 5,
+                                        }}>ServingSize</Text>
+                                       <Picker selectedValue={this.state.servingSize} onValueChange={value => this.setState({servingSize:value})} >
+                                            <Picker.Item label="1" value="1" />
+                                            <Picker.Item label="2" value="2" />
+                                            <Picker.Item label="3" value="3" />
+                                            <Picker.Item label="4" value="4" />
+                                            <Picker.Item label="5" value="5" />
+                                            <Picker.Item label="6" value="6" />
+                                            <Picker.Item label="7" value="7" />
+                                            <Picker.Item label="8" value="8" />
+                                            <Picker.Item label="9" value="9" />
+                                            <Picker.Item label="10" value="10" />
+                                        </Picker>    
+
                                  </Card>
 
                     </View>
@@ -73,5 +85,7 @@ class FoodCard extends Component {
         )
     }
 }
+
+
 
 export default FoodCard;
