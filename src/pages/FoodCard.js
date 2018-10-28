28208c2bf1d2b52580d7e786fda20a39f8535848
Vishal Karande <vishalkarande4@gmail.com>
2018-10-28 12:01:34 -0400

    serving size picker removed

--- src/pages/FoodCard.js
+++ src/pages/FoodCard.js
@@ -214,24 +214,3 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, {addfood,removefood}) (FoodCard);
 
-/*
-              <Card flexDirection='column'>
-                                        <Text style={{
-                                            color: "maroon",
-                                            fontSize: 15,
-                                            marginBottom: 5,
-                                        }}>ServingSize</Text>
-                                       <Picker selectedValue={this.state.servingSize} onValueChange={value => this.setState({servingSize:value})} >
-                                            <Picker.Item label="1" value="1" />
-                                            <Picker.Item label="2" value="2" />
-                                            <Picker.Item label="3" value="3" />
-                                            <Picker.Item label="4" value="4" />
-                                            <Picker.Item label="5" value="5" />
-                                            <Picker.Item label="6" value="6" />
-                                            <Picker.Item label="7" value="7" />
-                                            <Picker.Item label="8" value="8" />
-                                            <Picker.Item label="9" value="9" />
-                                            <Picker.Item label="10" value="10" />
-                                        </Picker>    
-                                 </Card>
-*/
