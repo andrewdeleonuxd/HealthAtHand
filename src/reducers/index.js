96ba963a9d1d0e355cccbce20595d97795d8b1b5
Vishal Karande <vishalkarande4@gmail.com>
2018-10-27 20:51:26 -0400

    add food and add exercise all issue resolved

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
 import {combineReducers} from 'redux';
 import AuthReducer from './AuthReducer';
-import EmployeeFormReducer from './EmployeeFormReducer';
-import EmployeeReducer from './EmployeeReducer';
 import FoodReducer from './FoodReducer';
 import ExerciseReducer from './ExerciseReducer';
 
@@ -10,7 +8,5 @@ import ExerciseReducer from './ExerciseReducer';
 export default combineReducers({
  exercise: ExerciseReducer,   
  food: FoodReducer,   
- auth: AuthReducer,
- employeeForm: EmployeeFormReducer,
- employees: EmployeeReducer
+ auth: AuthReducer
 });
